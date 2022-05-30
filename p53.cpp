4671132ad2a421f705814d3a396889e2cf347e69
RoshanShaikh <12roshanshaikh@gmail.com>
2022-05-30 23:26:06 +0530

    30-5-22

---
+++ p53.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution
+{
+public:
+    int maxSubArray(vector<int> &nums)
+    {
+        int ans = INT_MIN;
+        int cur = 0;
+        for (int i = 0; i < nums.size(); i++)
+        {
+            cur += nums[i];
+            ans = max(ans, cur);
+            if (cur < 0)
+                cur = 0;
+        }
+        return ans;
+    }
+};
+
+int main()
+{
+
+    return 0;
+}
